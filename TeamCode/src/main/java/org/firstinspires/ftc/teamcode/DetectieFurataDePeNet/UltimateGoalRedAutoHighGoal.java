e4081d2a677f3d98c455167d4434e0f56406163a
Robert <robertdamian2003@yahoo.com>
2021-06-03 19:03:12 +0300

    Incercati codul asta!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectieFurataDePeNet/UltimateGoalRedAutoHighGoal.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.DetectieFurataDePeNet;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name = "RedAutoHighGoal", group = "Autonomous")
+public class UltimateGoalRedAutoHighGoal extends LinearOpMode {
+
+    int stack = 2;
+    OpenCvWebcam webCam;
+
+    public void runOpMode(){
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webCam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+        webCam.openCameraDevice();
+
+        UltimateGoalReturnPositionPipeline pipeline = new UltimateGoalReturnPositionPipeline();
+
+        webCam.setPipeline(pipeline);
+        webCam.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT);
+        webCam.resumeViewport();
+
+        FtcDashboard.getInstance().startCameraStream(webCam, 0);
+
+        sleep(2000);
+        stack = pipeline.stack;
+        telemetry.addData("stack",stack);
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectieFurataDePeNet/UltimateGoalReturnPositionPipeline.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.DetectieFurataDePeNet;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class UltimateGoalReturnPositionPipeline extends OpenCvPipeline {
+    public int BOTTOMLEFTX = 550;
+    public int BOTTOMLEFTY = 254;//bot left coordinates of small 1 ring box
+    public int WIDTH = 68;//width of 1 ring box
+    public int HEIGHT1 = 31;//height of 1 ring
+    public int BUFFER = 12;
+    public int HEIGHT2 = 48;//height of 4 rings
+    public double HThresholdLow = 5;
+    public double HThresholdHigh = 18;
+    public int stack;
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat HSVmat = new Mat();
+        Imgproc.rectangle(input, new Point(BOTTOMLEFTX,BOTTOMLEFTY), new Point(BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT1), new Scalar(255,255,255),1);
+        Imgproc.rectangle(input, new Point(BOTTOMLEFTX,BOTTOMLEFTY+HEIGHT1+BUFFER), new Point(BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT2), new Scalar(255,255,255),1);
+        Imgproc.cvtColor(input,HSVmat,Imgproc.COLOR_RGB2HSV);
+        double[] topBox = Core.mean(HSVmat.submat(new Rect(new Point(BOTTOMLEFTX,BOTTOMLEFTY), new Point(BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT1)))).val;
+        //getAverageYCrCb(yCrCbmat, BOTTOMLEFTX,BOTTOMLEFTY,BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT1);
+        double[] bottomBox = Core.mean(HSVmat.submat(new Rect(new Point(BOTTOMLEFTX,BOTTOMLEFTY+HEIGHT1+BUFFER), new Point(BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT2)))).val;
+        //getAverageYCrCb(yCrCbmat, BOTTOMLEFTX,BOTTOMLEFTY+HEIGHT1,BOTTOMLEFTX+WIDTH,BOTTOMLEFTY+HEIGHT2);
+        Imgproc.putText(input,"TopBoxH: "+topBox[0],new Point(0,100),1,1,new Scalar(0,0,0));
+        Imgproc.putText(input,"BottomBoxH: "+bottomBox[0],new Point(0,200),1,1,new Scalar(0,0,0));
+        if(isAboveThresholds(topBox[0])){
+            stack = 2;
+        }
+        else if(isAboveThresholds(bottomBox[0])){
+            stack = 1;
+        }
+        else{
+            stack = 0;
+        }
+        return input;
+    }
+
+    public boolean isAboveThresholds(double avgH){
+        return avgH > HThresholdLow && avgH < HThresholdHigh;
+    }
+}
